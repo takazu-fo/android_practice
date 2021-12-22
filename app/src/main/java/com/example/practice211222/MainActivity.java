a210477d2de2b77086ac87325f78f3e32642c334
Takahashi <kazuya.takahashi@fox-hound.tech>
2021-12-22 19:28:29 +0900

    フラグメントの練習
    コマンドでコメント売ってみたけどどんな感じ？

--- app/src/main/java/com/example/practice211222/MainActivity.java
+++ app/src/main/java/com/example/practice211222/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.practice211222;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            // FragmentManagerのインスタンス生成
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            // FragmentTransactionのインスタンスを取得
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            // インスタンスに対して張り付け方を指定する
+            fragmentTransaction.replace(R.id.container, new SampleFragment());
+
+            // 張り付けを実行
+            fragmentTransaction.commit();
+        }
     }
 }
